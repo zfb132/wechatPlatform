d828160a3419eb58511c78936b9aa6d5866942d8
zfb132 <zfb132@gmail.com>
2019-01-14 15:40:11 +0800

    添加命令菜单

--- app/model/Menu.py
+++ app/model/Menu.py
@@ -7,13 +7,16 @@ import random
 from app.config import DBNAME, DBPWD
 
 # 命令列表
-# 0  未知命令
+# 0  菜单
 # 1  随机返回5个中文名字
 # 2  随机返回五个日本名字
 # 3  随机返回五个英语名字
 # 4  随机返回五个成语
 # 5  成语接龙
 def getcommand(content):
+    # 如果是单个数字直接解析命令
+    if len(content)==1 and content.isdigit():
+        return int(content)
     if '名字' in content:
         if '中国' in content or "中文" in content:
             return 1
@@ -25,14 +28,20 @@ def getcommand(content):
             return 1
     else:
         if '成语' in content:
-            return 4
+            if '接龙' in content:
+                return 5
+            else:
+                return 4
         else:
-            return 0
+            if '命令' in content or '菜单' in content:
+                return 0
+            else:
+                return 9
 
 def parsecontent(content):
     command = getcommand(content)
     if command == 0:
-        txt = defaultReply(content)
+        txt = menuReply()
     elif command == 1:
         txt = randomdata('chinese',1896600)
     elif command == 2:
@@ -42,7 +51,9 @@ def parsecontent(content):
     elif command == 4:
         txt = randomdata('idiom',50370)
     elif command == 5:
-        txt = errorReply(content)
+        txt = tempReply(content)
+    else:
+        txt = defaultReply(content)
     return txt
 
 
@@ -64,7 +75,12 @@ def randomdata(tablename,maxID):
     db.close()
     return ', '.join(result)
 
-def errorReply(content):
+
+def menuReply():
+    txt = '命令列表\n0  菜单\n1  随机返回5个中文名字\n2  随机返回五个日本名字\n3  随机返回五个英语名字\n4  随机返回五个成语\n5  成语接龙'
+    return txt
+
+def tempReply(content):
     return '你发送了 :{}'.format(content)
 
 # 伪对话AI
