815f7d98effcbdbfe62146dc43f58d1559c1ca4d
zfb132 <zfb132@gmail.com>
2019-01-14 16:43:07 +0800

    添加重启脚本

---
+++ restart.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+PORT=8111
+kill -9  $(lsof -t -i:8111) > /dev/null 2>&1
+if [ $? -eq 0 ];then
+    echo "Kill $PORT port successfully!"
+else
+    echo "Fail to kill $PORT port"
+fi
+NAME=~/wechatPlatform/server.log
+uwsgi uwsgi_wechat.ini -d $NAME > /dev/null 2>&1
+if [ $? -eq 0 ];then
+    echo "Restart successfully!"
+else
+    echo "Fail to restart"
+fi
